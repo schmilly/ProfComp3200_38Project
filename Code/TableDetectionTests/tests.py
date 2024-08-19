9a820cf994c730924ba850a002d587830be1927b
schmilly <william.f.s.bake@gmail.com>
2024-08-19 22:51:22 +0800

    further experimentation with table detection

---
+++ Code/TableDetectionTests/tests.py
@@ -0,0 +1,8 @@
+from luminositybased import *
+
+# Load image
+print(findTable('/home/schmilly/ProfComp3200_38Project/Examples/PageExportCropped.png',"borderless","borderless"))
+print(findTable('/home/schmilly/ProfComp3200_38Project/Examples/IndiaPageExport.png',"borderless","borderless"))
+print(findTable('/home/schmilly/ProfComp3200_38Project/Examples/MoroccoPageExport.png',"borderless","borderless"))
+print(findTable('/home/schmilly/ProfComp3200_38Project/Examples/IsraelPageExport.png',"borderless","border",1/100,1/100))
+
